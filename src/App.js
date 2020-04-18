942c2595a3994191672954798eb573134f860f88
Kendall Brown <kndllbrwn1914@gmail.com>
2020-04-17 22:26:17 -0400

    Working App using hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Launches from "./components/Launches";
+import Launch from "./components/Launch";
+import "./App.css";
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql?",
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="container">
+          <h1>SpaceX</h1>
+          <Route exact path="/" component={Launches} />
+          <Route exact path="/launch/:flight_number" component={Launch} />
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
